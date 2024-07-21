b945f6929759a1312aac9a9031fc8b9d1c2e181d
Caner Iskenderoglu <caner.iskenderoglu@gmx.de>
2024-07-21 13:26:41 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        DEFAULT: '#635FC7',
+        light: '#A8A4FF',
+      },
+      danger: {
+        DEFAULT: '#EA5555',
+        light: '#FF9898',
+      },
+      neutral: {
+        DEFAULT: '#828FA3',
+        light: '#F4F7FD',
+        dark: '#2B2C37',
+        darker: '#20212C',
+      },
+      lines: {
+        dark: '#3E3F4E',
+        light: '#E4EBFA',
+      },
+      black: '#000112',
+      white: '#FFFFFF',
+    },
   },
   plugins: [],
 };
