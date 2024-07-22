af01a98111eed973c248c941935e008a4ff0c855
Caner Iskenderoglu <caner.iskenderoglu@gmx.de>
2024-07-22 20:07:48 +0200

    using prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
