4f959078a5411b73cfefc8c316e489869bcbd336
Caner Iskenderoglu <caner.iskenderoglu@gmx.de>
2024-07-22 20:29:27 +0200

    add husky and lint-staged for pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="read-the-docs">
+      <p className="read-the-docs  ">
         Click on the Vite and React logos to learn more
       </p>
     </>
