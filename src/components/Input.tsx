262f62036d141cc2511bfb8e61def2e079d3634a
Caner Iskenderoglu <caner.iskenderoglu@gmx.de>
2024-09-16 17:14:09 +0200

    using react-hook-form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,15 @@
-import { ComponentProps } from 'react';
+import { ComponentProps, forwardRef } from 'react';
 
 type InputProps = ComponentProps<'input'>;
-export function Input(props: InputProps) {
+// we need this so that we can call {...register("fieldName")} from outside
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   return (
     <input
       {...props}
+      ref={ref}
       placeholder={props.placeholder}
       type="text"
       className={`${props.className} rounded border border-neutral border-opacity-25 px-4 py-2 text-base placeholder:opacity-25 text-black`}
     />
   );
-}
+});
--- src/features/boards/AddBoardForm.tsx
+++ src/features/boards/AddBoardForm.tsx
@@ -2,32 +2,60 @@ import { IconCross } from '@/assets/IconCross';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 import { Label } from '@/components/Label';
+import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
+
+interface Column {
+  columnName: string;
+}
+
+interface FormValues {
+  name: string;
+  columns: Column[];
+}
 
 export function AddBoardForm() {
+  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
+
+  const { register, handleSubmit, control } = useForm<FormValues>({
+    defaultValues: {
+      name: 'Harry potter',
+      columns: [{ columnName: 'Todo' }, { columnName: 'Doing' }],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'columns',
+  });
+
   return (
-    <form className="flex flex-col gap-6 w-[480px] p-8">
-      <h2 className="text-lg">Edit Board</h2>
+    <form
+      className="flex flex-col gap-6 w-[480px] p-8"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h2 className="text-lg">Add Board</h2>
       <div className="flex flex-col gap-6">
         <div className="flex flex-col gap-2">
           <Label>Name</Label>
-          <Input placeholder="e.g Web Design" />
+          <Input placeholder="e.g Web Design" {...register('name')} />
         </div>
         <div className="flex flex-col gap-2">
           <Label>Columns</Label>
-          <div className="flex gap-2">
-            <Input value="Todo" className="flex-1" />
-            <Button variant="inline">
-              <IconCross />
-            </Button>
-          </div>
-          <div className="flex gap-2">
-            <Input value="Doing" className="flex-1" />
-            <Button variant="inline">
-              <IconCross />
-            </Button>
-          </div>
+          {fields.map((field, index) => (
+            <div className="flex gap-2" key={field.id}>
+              <Input
+                className="flex-1"
+                {...register(`columns.${index}.columnName`)}
+              />
+              <Button variant="inline" onClick={() => remove(index)}>
+                <IconCross />
+              </Button>
+            </div>
+          ))}
         </div>
-        <Button variant="secondary">+ Add New Column</Button>
+        <Button variant="secondary" onClick={() => append({ columnName: '' })}>
+          + Add New Column
+        </Button>
       </div>
       <Button variant="primary">Create new Board</Button>
     </form>
