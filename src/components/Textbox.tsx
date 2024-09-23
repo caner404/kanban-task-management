9f788c81d1e438916ac261fcfc6b7935cc2d281e
Caner Iskenderoglu <caner.iskenderoglu@gmx.de>
2024-09-23 15:02:02 +0200

    fix modalview

--- src/components/Textbox.tsx
+++ src/components/Textbox.tsx
@@ -1,23 +1,33 @@
 import { ComponentProps, forwardRef } from 'react';
 import { Label } from './Label';
 
-type TextboxLabelProps = { label: string; id: string };
+type TextboxLabelProps = { label?: string; id?: string };
 type TextboxProps = ComponentProps<'input'> & Partial<TextboxLabelProps>;
 
 export const Textbox = forwardRef<HTMLInputElement, TextboxProps>(
   ({ label, id, ...props }, ref) => {
+    if (label)
+      return (
+        <div className="flex flex-col gap-2">
+          {label && <Label htmlFor={id}>{label}</Label>}
+          <input
+            {...props}
+            ref={ref}
+            id={id}
+            placeholder={props.placeholder}
+            type="text"
+            className={`${props.className} rounded border border-neutral border-opacity-25 px-4 py-2 text-base placeholder:opacity-25 text-black`}
+          />
+        </div>
+      );
     return (
-      <div className="flex flex-col gap-2">
-        {label && <Label htmlFor={id}>{label}</Label>}
-        <input
-          {...props}
-          ref={ref}
-          id={id}
-          placeholder={props.placeholder}
-          type="text"
-          className={`${props.className} rounded border border-neutral border-opacity-25 px-4 py-2 text-base placeholder:opacity-25 text-black`}
-        />
-      </div>
+      <input
+        {...props}
+        ref={ref}
+        placeholder={props.placeholder}
+        type="text"
+        className={`${props.className} rounded border border-neutral border-opacity-25 px-4 py-2 text-base placeholder:opacity-25 text-black`}
+      />
     );
   },
 );
